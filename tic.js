bdf44d180cd9fef957e0ea28c7bb612a4e66b114
ZCPower <zachpowerxc@gmail.com>
2022-04-06 09:17:41 -0500

    Added functionality to detect when a winning combination has been played on the map. Player is no longer able to click once a winner has been declared. Now i just need to make the computer make moves and add an animation or coloring on the board along the winning combination

--- tic.js
+++ tic.js
@@ -12,6 +12,7 @@ const board = document.getElementById('gameContainer');
 const buttonContainer = document.getElementById('buttonContainer');
 const mediumDisplay = window.matchMedia('(max-width: 1280px)')
 const largeDisplay = window.matchMedia('(min-width: 1281px')
+const cell0 = document.getElementById('cell0');
 const cell1 = document.getElementById('cell1');
 const cell2 = document.getElementById('cell2');
 const cell3 = document.getElementById('cell3');
@@ -20,10 +21,25 @@ const cell5 = document.getElementById('cell5');
 const cell6 = document.getElementById('cell6');
 const cell7 = document.getElementById('cell7');
 const cell8 = document.getElementById('cell8');
-const cell9 = document.getElementById('cell9');
 const gameContainer = document.getElementById('gameContainer')
 const gameTurnTracker = document.createElement('p')
 gameContainer.appendChild(gameTurnTracker);
+
+//replay button
+const replayButton = document.createElement('button');
+replayButton.innerText = 'Replay!'
+replayButton.style.padding = '2em'
+replayButton.style.backgroundColor = 'red'
+replayButton.style.marginTop = '6vh'
+replayButton.style.color = 'white'
+
+//newGame Button
+const newGameButton = document.createElement('button');
+newGameButton.innerText = 'Game Mode Selection'
+newGameButton.style.padding = '2em'
+newGameButton.style.backgroundColor = 'red'
+newGameButton.style.marginTop = '6vh'
+newGameButton.style.color = 'white'
 
 //variables for sounds
 const error = document.getElementById('error')
@@ -35,18 +51,18 @@ function clickSound() {
     click.play()
 }
 let playerCount = 0; //This variable will determine whether to continue on in playerSelection functions
-
 //starting game object
 game = {
-    playerOne: { name: player1Name.value, symbol: 'X' },
-    playerTwo: { name: player2Name.value, symbol: 'O' },
-    gameGrid: 
-        [
-            [cell1.innerHTML, '', ''], // Row 0
-            ['', '', ''], // Row 1
-            ['', '', '']
-        ],
+    playerOne: { name: player1Name.value, mark: 'X' },
+    playerTwo: { name: player2Name.value, mark: 'O' },
+    gameGrid:
+        [],
+    winner: ''
 }
+
+// [][]
+
+// gameGrid[0][1] = cell2.innerText
 
 //selects a single player mode
 function singlePlayerMode() {
@@ -130,7 +146,7 @@ function generateBoard(event) {
         } else {
             errorSound()
             h2.innerText = 'Please choose a name with a length of 1-16'
-            h2.style.color ='yellow'
+            h2.style.color = 'yellow'
         }
     }
 }
@@ -138,7 +154,7 @@ function generateBoard(event) {
 
 let turnCount = 0;
 function startingPlayer() {
-    turnCount = Math.floor(Math.random() * 2) + 1;
+    turnCount = Math.floor(Math.random() * 2);
     console.log(turnCount)
     if (turnCount % 2 === 0) {
         console.log(`It is ${game.playerOne.name}'s turn!`)
@@ -150,6 +166,17 @@ function startingPlayer() {
     }
 }
 
+function updateGameArray() {
+    game.gameGrid[0] = cell0.innerText;
+    game.gameGrid[1] = cell1.innerText;
+    game.gameGrid[2] = cell2.innerText;
+    game.gameGrid[3] = cell3.innerText;
+    game.gameGrid[4] = cell4.innerText;
+    game.gameGrid[5] = cell5.innerText;
+    game.gameGrid[6] = cell6.innerText;
+    game.gameGrid[7] = cell7.innerText;
+    game.gameGrid[8] = cell8.innerText;
+}
 
 
 // if (turnCount % 2 === 0) gameTurnTracker.innerText = `It is ${game.playerOne.name}'s turn!`
@@ -157,22 +184,29 @@ function startingPlayer() {
 
 
 //On click edit the innerText of the cell. Check
-    //add the innerText to the game corresponding game grid spot. 
+//add the innerText to the game corresponding game grid spot. 
 
 function addLetter(event) {
-    if (event.target.innerText === '' || event.target.innerText === undefined){
-        if (turnCount % 2 === 0) event.target.innerText = game.playerOne.symbol;
-        else event.target.innerText = game.playerTwo.symbol;
+    if (event.target.innerText === '' || event.target.innerText === undefined) {
         turnCount++
         clickSound()
-        if (turnCount % 2 === 0) gameTurnTracker.innerText = `It is ${game.playerOne.name}'s turn!`
-        if (turnCount % 2 === 1) gameTurnTracker.innerText = `It is ${game.playerTwo.name}'s turn!`
-        console.log(turnCount)
-        console.log(event.target)
-        console.log(event.target.innerText)
-        console.log(game.gameGrid)
-        
-}
+        if (turnCount % 2 === 1) {
+            gameTurnTracker.innerText = `It is ${game.playerTwo.name}'s turn!`
+            event.target.innerText = game.playerOne.mark;
+            updateGameArray()
+            checkForWin()
+        }
+        if (turnCount % 2 === 0) {
+            gameTurnTracker.innerText = `It is ${game.playerOne.name}'s turn!`
+            event.target.innerText = game.playerTwo.mark;
+            updateGameArray()
+            checkForWin()
+        }
+        // console.log(turnCount)
+        // console.log(event.target)
+        // console.log(event.target.innerText)
+        // console.log(game.gameGrid)
+    }
 }
 
 
@@ -182,47 +216,143 @@ twoPlayer.addEventListener('click', twoPlayerMode)
 player1Name.addEventListener('keydown', changePlayerOneName)
 player2Name.addEventListener('keydown', generateBoard)
 board.addEventListener('click', addLetter)
-
-//
-
+replayButton.addEventListener('click', replay)
 
 
-// // Check if the media query is true
-// if (smallLaptop.matches) {
-//     // Then trigger an alert
-//     alert('You are on your little laptop')
-// } 
-
-// if (largeScreen.matches) {
-//     alert('Big Screen livin')
-// }
-
-//Have the players symbol display in a each cell on mouse over. Only if the cell is unoccupied.
-    //On click the symbol will be placed. Only if cell is unoccupied
+//winning combos
+function checkForWin() {
+    checkRows();
+    checkColumns();
+    checkDiagonals();
+}
 
 
+//a check to see if there is a winner after every turn. Could add a way to that indicates a winner on board i.e. a line through the section OR change color of text of the winning line.
+function checkRow1() {
+    if (game.gameGrid[0] === 'X' && game.gameGrid[1] === 'X' && game.gameGrid[2] === 'X') playerOneWins()
+    if (game.gameGrid[0] === 'O' && game.gameGrid[1] === 'O' && game.gameGrid[2] === 'O') playerTwoWins()
+}
+function checkRow2() {
+    if (game.gameGrid[3] === 'X' && game.gameGrid[4] === 'X' && game.gameGrid[5] === 'X') playerOneWins()
+    if (game.gameGrid[3] === 'O' && game.gameGrid[4] === 'O' && game.gameGrid[5] === 'O') playerTwoWins()
+}
+function checkRow3() {
+    if (game.gameGrid[6] === 'X' && game.gameGrid[7] === 'X' && game.gameGrid[8] === 'X') playerOneWins()
+    if (game.gameGrid[6] === 'O' && game.gameGrid[7] === 'O' && game.gameGrid[8] === 'O') playerTwoWins()
+}
+
+function checkColumn1() {
+    if (game.gameGrid[0] === 'X' && game.gameGrid[3] === 'X' && game.gameGrid[6] === 'X') playerOneWins()
+    if (game.gameGrid[0] === 'O' && game.gameGrid[3] === 'O' && game.gameGrid[6] === 'O') playerTwoWins()
+}
+
+function checkColumn2() {
+    if (game.gameGrid[1] === 'X' && game.gameGrid[4] === 'X' && game.gameGrid[7] === 'X') playerOneWins()
+    if (game.gameGrid[1] === 'O' && game.gameGrid[4] === 'O' && game.gameGrid[7] === 'O') playerTwoWins()
+}
+
+function checkColumn3() {
+    if (game.gameGrid[2] === 'X' && game.gameGrid[5] === 'X' && game.gameGrid[8] === 'X') playerOneWins()
+    if (game.gameGrid[2] === 'O' && game.gameGrid[5] === 'O' && game.gameGrid[8] === 'O') playerTwoWins()
+}
+
+function checkDiagonal1() {
+    if (game.gameGrid[0] === 'X' && game.gameGrid[4] === 'X' && game.gameGrid[8] === 'X') playerOneWins()
+    if (game.gameGrid[0] === 'O' && game.gameGrid[4] === 'O' && game.gameGrid[8] === 'O') playerTwoWins()
+}
+
+function checkDiagonal2() {
+    if (game.gameGrid[2] === 'X' && game.gameGrid[4] === 'X' && game.gameGrid[6] === 'X') playerOneWins()
+    if (game.gameGrid[2] === 'O' && game.gameGrid[4] === 'O' && game.gameGrid[6] === 'O') playerTwoWins()
+}
+
+function checkRows() {
+    checkRow1();
+    checkRow2();
+    checkRow3();
+}
+
+function checkColumns() {
+    checkColumn1();
+    checkColumn2();
+    checkColumn3();
+}
+
+function checkDiagonals() {
+    checkDiagonal1();
+    checkDiagonal2();
+}
 
 
-//Opening screen will have player select game mode: 1 player or 2 player. 
-    //have player select one or two
-        //if singlePlayer have the heading change to What is your name and take that input and store it in an object. Player can decide if they are X or O. 
+
+function playerOneWins() {
+    h2.innerText = `${game.playerOne.name} is the winner!`
+    h2.style.color = 'green'
+    gameContainer.appendChild(replayButton);
+    gameTurnTracker.remove();
+    board.style.pointerEvents = 'none'
+    replayButton.style.pointerEvents = 'auto'
+}
+
+function playerTwoWins() {
+    h2.innerText = `${game.playerTwo.name} is the winner!`
+    if (playerCount === 1) h2.style.color = 'red'
+    else h2.style.color = 'green'
+    gameContainer.appendChild(replayButton)
+    gameTurnTracker.remove();
+    board.style.pointerEvents = 'none'
+    replayButton.style.pointerEvents = 'auto'
+    //need to create a way that checks the player count. if player two wins and player count = 1, the text should be red. 
+}
+
+function newGame() {
+    gameModeSelection();
+}
+
+function replay() {
+    clearGameArray();
+    reseth2Text();
+    replayButton.remove();
+    gameContainer.appendChild(gameTurnTracker)
+    startingPlayer()
+    board.style.pointerEvents = 'auto'
+}
+
+//Clears the game board.
+function clearGameArray() {
+    game.gameGrid = [];
+    cell0.innerText = '';
+    cell1.innerText = '';
+    cell2.innerText = '';
+    cell3.innerText = '';
+    cell4.innerText = '';
+    cell5.innerText = '';
+    cell6.innerText = '';
+    cell7.innerText = '';
+    cell8.innerText = '';
+}
+
+function gameEndingSound() {
+
+}
+
+function reseth2Text() {
+    h2.innerText = `${game.playerOne.name} vs. ${game.playerTwo.name}`
+    h2.style.color = 'white'
+}
+
+
+//create a way for computer to play
+function computerTurn() {
+    let computerCell = ''
+    if (playerCount = 1 && turnCount % 2 === 1) {
+        computerCell = Math.floor(Math.random() * 8);
+        gameGrid[computerCell] = 'O';
+        cell[computerCell].innerHTML = 'O'
+    }
+}
 
 
 
-    //if 1 player - have player select name and player 2 will be named "Computer"
-    //Computer will have to make moves that aren't just random rolls.
-        //if (playerturn = [x]) {
-            // computerturn =[o] on a corresponding square to block next move set up
-        // }
-
-    
-    //if 2 player - each player will select their name and it will appear on the screen
-    //order will be randomly generated
-        // would like to have a coin flipping animation and have the odds be 1/2 to dictate who goes first.
-    //turns will alternate
-    //only be able to place marks in unooccupied spaces
-        //if (space !== undefined) {
-            // insert players mark
-        // }
     //create a winning notification and animation
     //restart game without having to reset the browser
